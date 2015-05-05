bb43c7fcb665ec7b95da030920409c443a77ea6e
yyamnk <y.yamnk@gmail.com>
2015-05-06 00:31:17 +0900

    アプリ名変更

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Sample Admin Roles"
+  config.site_title = "参加団体管理ページ"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
