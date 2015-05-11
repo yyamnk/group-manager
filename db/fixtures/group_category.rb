c0c4dec1dfaec8471e5d60e74de82f48b9bde002
yyamnk <y.yamnk@gmail.com>
2015-05-11 19:11:05 +0900

    mod: GroupCategoryの初期値, 模擬店 -> 模擬店(食品販売), 模擬店(物品販売)

--- db/fixtures/group_category.rb
+++ db/fixtures/group_category.rb
@@ -1,6 +1,7 @@
 GroupCategory.seed( :id,
-  { id: 1, name_ja: '模擬店' },
-  { id: 2, name_ja: 'ステージ企画' },
-  { id: 3, name_ja: '展示' },
-  { id: 4, name_ja: 'その他' }
+  { id: 1, name_ja: '模擬店(食品販売)' },
+  { id: 2, name_ja: '模擬店(物品販売)' },
+  { id: 3, name_ja: 'ステージ企画' },
+  { id: 4, name_ja: '展示' },
+  { id: 5, name_ja: 'その他' }
 )
