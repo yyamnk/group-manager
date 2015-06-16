14b4d70c12cebb67cdc0ce171677f942b203f18c
yyamnk <y.yamnk@gmail.com>
2015-06-16 21:52:08 +0900

    mod: models/user, 初期登録を不許可に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :registerable, :confirmable
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    # :registerable, # 登録を不可に変更
+    :confirmable
+
   belongs_to :role      # Userからroleを参照可能にする, ex) User.find(1).role
   has_one :user_detail  # UserからUserDetailを参照可能にする
   has_many :groups
