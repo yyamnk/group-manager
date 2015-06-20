d8809762a72649b32f4dc40bc6b856e055bc6ef0
yyamnk <y.yamnk@gmail.com>
2015-06-20 22:38:16 +0900

    mod: app/adimn/stage_order, activeadminからレコードの変更を許可

--- app/admin/stage_order.rb
+++ app/admin/stage_order.rb
@@ -1,18 +1,6 @@
 ActiveAdmin.register StageOrder do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  permit_params :group_id, :fes_date_id, :is_sunny, :stage_first, :stage_second, :time, :own_equipment, :bgm, :camera_permittion, :loud_sound
 
   index do
     selectable_column
