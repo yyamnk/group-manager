87f1fdb2863a298841b31dcd63e54774abe2badd
yyamnk <y.yamnk@gmail.com>
2015-06-27 20:54:22 +0900

    mod: views/food_products/show, 表示カラム変更，モデルに調理の有無を表示するメソッド追加

--- app/models/food_product.rb
+++ app/models/food_product.rb
@@ -5,4 +5,10 @@ class FoodProduct < ActiveRecord::Base
   validates_numericality_of :group_id, :num
   # boolean型をチェック. presence_of ではfalseでエラーになってしまう
   validates :is_cooking, inclusion: {in: [true, false]}
+
+  def disp_cooking
+    disp = self.is_cooking ? '調理あり' : '調理なし(提供のみ)'
+    return disp
+  end
+
 end
