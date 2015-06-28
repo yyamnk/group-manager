41d88a881e9b3dd8123a8a14745a77ea590b399a
yyamnk <y.yamnk@gmail.com>
2015-06-29 02:00:46 +0900

    add: db/fixtures/shop, Shopの初期データを投入

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,9 @@
 class Shop < ActiveRecord::Base
+
+  serialize :closed # 配列を格納できるようにする.
+  validates_presence_of :name, :kana, :tel
+
+  # tel -> 半角数字とハイフンのみ, [4444-22-4444, for 固定] )
+  validates :tel,     format: { with: /(\A\d{4}-\d{2}-\d{4})+\z/i }
+
 end
