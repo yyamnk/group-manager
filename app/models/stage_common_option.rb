873c4a86d5d0ea0485ee272648251fad152320a6
kkoike <k.koike03@gmail.com>
2016-01-13 22:27:49 +0900

    upd: 出演内容は必須入力に

--- app/models/stage_common_option.rb
+++ app/models/stage_common_option.rb
@@ -2,7 +2,7 @@ class StageCommonOption < ActiveRecord::Base
   belongs_to :group
 
   # 存在チェック
-  validates :group_id, presence: true
+  validates :group_id, :stage_content, presence: true
 
   # 団体毎にユニーク
   validates :own_equipment, :bgm, :camera_permittion, :loud_sound, :stage_content, :uniqueness => {:scope => [:group_id] }
