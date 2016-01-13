c723df33f36678a07eb7c895621760868aa2aed5
kkoike <k.koike03@gmail.com>
2016-01-13 20:57:27 +0900

    add: Varidate stage_common_option

--- app/models/stage_common_option.rb
+++ app/models/stage_common_option.rb
@@ -1,3 +1,10 @@
 class StageCommonOption < ActiveRecord::Base
   belongs_to :group
+
+  # 存在チェック
+  validates :group_id, presence: true
+
+  # 団体毎にユニーク
+  validates :own_equipment, :bgm, :camera_permittion, :loud_sound, :stage_content, :uniqueness => {:scope => [:group_id] }
+
 end
