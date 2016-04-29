0f0662495809c350c7cda393fa9be33fd13a1317
kkoike <k.koike03@gmail.com>
2016-04-30 05:42:11 +0900

    del: 怖いのでstage_orderの共通項目カラム削除用のmigrationファイルを削除

--- db/migrate/20160216092753_remove_common_option_to_stage_orders.rb
+++
@@ -1,8 +0,0 @@
-class RemoveCommonOptionToStageOrders < ActiveRecord::Migration
-  def change
-    remove_column :stage_orders, :own_equipment
-    remove_column :stage_orders, :bgm
-    remove_column :stage_orders, :camera_permittion
-    remove_column :stage_orders, :loud_sound
-  end
-end
