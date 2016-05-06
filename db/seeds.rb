9d718707ac504e85e156443a2e056ac34a4cc186
TKNGUE <takeno@jnlp.org>
2016-05-06 12:34:31 +0900

    Update #37 Viewの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.new(:email => 'guest@nutfes.com', :password => 'guest')
-user.save!
