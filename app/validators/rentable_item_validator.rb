c64279cf1c904c48973456b821c1f78cfd5c7252
yyamnk <y.yamnk@gmail.com>
2016-05-10 00:40:39 +0900

    fix: RentableItemValidator, nilに対応

--- app/validators/rentable_item_validator.rb
+++ app/validators/rentable_item_validator.rb
@@ -1,7 +1,9 @@
 class RentableItemValidator < ActiveModel::Validator
 
   def validate(record)
-    if record.max_num > record.stocker_item.num
+    if record.stocker_item.blank? || record.max_num.blank?
+      return
+    elsif record.max_num > record.stocker_item.num
       msg = '貸出可能数が登録済みの在庫数を超えています'
       record.errors[:max_num] << msg
     end
