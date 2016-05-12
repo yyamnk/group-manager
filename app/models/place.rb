10085e61d7447121b9e8589d9725b17b700534ad
ShuzoN <d.namu.r21@gmail.com>
2016-05-12 19:08:28 +0900

    upd: models/place, modify form. 「実施場所の申請」フォームをPlaceAllowListに対応

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,26 +1,21 @@
 class Place < ActiveRecord::Base
   has_many :place_allow_lists
 
+  # 団体カテゴリ毎に利用可能な場所を検索する
+  scope :search_enable_place, ->(group_category_id){
+    PlaceAllowList.where(group_category_id: group_category_id, enable: true).map {|allow_place|
+      self.find(allow_place.place_id)
+    }
+
+  }
 
   def to_s # aciveAdmin, simple_formで表示名を指定する
-    if self.is_outside
       self.name_ja
-    else
-      self.name_ja + " (屋内)"
-    end
   end
 
   # 選択肢の生成用，団体idで選択可能な場所を返す
   def self.collections(group_id)
     group_category_id = Group.find(group_id).group_category_id
-    # 模擬店は外
-    if [1, 2].include? group_category_id
-      self.where( is_outside: true )
-    elsif [4, 5].include? group_category_id
-    # 展示・体験, その他は屋外 or 屋内
-      self.all
-    end
+    self.search_enable_place(group_category_id)
   end
-
-  #
 end
