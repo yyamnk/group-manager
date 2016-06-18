6bbe3ad8677a189ca548f8b5f9000d1a3a06e83b
TKNGUE <takeno@jnlp.org>
2016-06-18 10:36:35 +0900

    Fix reviews

--- db/fixtures/fes_year_date.rb
+++ db/fixtures/fes_year_date.rb
@@ -8,9 +8,9 @@ FesYear.seed( :id,
 )
 
 FesDate.seed( :id,
-  { id: 1, days_num:0, date:'9/16', day:'fri', fes_year_id: 1} ,
-  { id: 2, days_num:1, date:'9/17', day:'sat', fes_year_id: 1} ,
-  { id: 3, days_num:2, date:'9/18', day:'sun', fes_year_id: 1} ,
+  { id: 1, days_num:0, date:'9/19', day:'fri', fes_year_id: 1} ,
+  { id: 2, days_num:1, date:'9/20', day:'sat', fes_year_id: 1} ,
+  { id: 3, days_num:2, date:'9/21', day:'sun', fes_year_id: 1} ,
   { id: 4, days_num:0, date:'9/16', day:'fri', fes_year_id: 2} ,
   { id: 5, days_num:1, date:'9/17', day:'sat', fes_year_id: 2} ,
   { id: 6, days_num:2, date:'9/18', day:'sun', fes_year_id: 2} ,
