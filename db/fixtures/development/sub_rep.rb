e9476fc74ce4b102fd78bf97108687f6c490722a
TKNGUE <takeno@jnlp.org>
2016-06-30 20:15:18 +0900

    Fix #43 開発用データの追加
         - グループ
         - 副代表
         - ユーザ
         - ユーザ詳細   の開発環境用のシードを追加する

--- db/fixtures/development/sub_rep.rb
+++ db/fixtures/development/sub_rep.rb
@@ -1,32 +1,33 @@
-UserDetail.seed( :id,
+SubRep.seed( :id,
 {   id: 10000010,
-    user_id: User.where(:email => 'admin@nutfes.com').first.id,  # 上のidに対応
+    group_id: 1000014,  # 上のidに対応
     name_en: "Taro GIDAI admin",
     name_ja: "技大太郎 (admin)",
+    email: "user2@nutfes.com",
     department_id: 2,
     grade_id: 2,
     tel: "111-0000-9999",
-},
-{   id: 10000011,
-    user_id: User.where(:email => 'developer@nutfes.com').first.id,  # 上のidに対応
+},{   id: 10000011,
+    group_id: 1000013,  # 上のidに対応
     name_en: "Taro GIDAI admin",
     name_ja: "技大太郎 (admin)",
+    email: "user2@nutfes.com",
     department_id: 2,
     grade_id: 2,
     tel: "111-0000-9999",
-},
-{   id: 10000012,
-    user_id: User.where(:email => 'user1@nutfes.com').first.id,  # 上のidに対応
+}, {   id: 10000012,
+    group_id: 1000015,  # 上のidに対応
     name_en: "Taro GIDAI admin",
     name_ja: "技大太郎 (admin)",
+    email: "user2@nutfes.com",
     department_id: 2,
     grade_id: 2,
     tel: "111-0000-9999",
-},{   
-    id: 10000013,
-    user_id: User.where(:email => 'user1@nutfes.com').first.id,  # 上のidに対応
+}, {   id: 10000013,
+    group_id: 1000014,  # 上のidに対応
     name_en: "Taro GIDAI admin",
     name_ja: "技大太郎 (admin)",
+    email: "user2@nutfes.com",
     department_id: 2,
     grade_id: 2,
     tel: "111-0000-9999",
