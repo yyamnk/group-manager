97a373648889e20b2ccf153c4b244d433134db9a
kkoike <k.koike03@gmail.com>
2016-07-01 15:56:00 +0900

    add: 重複チェック済みの検便者一覧を表示するように

--- app/helpers/stool_test_pages_helper.rb
+++ app/helpers/stool_test_pages_helper.rb
@@ -1,2 +1,30 @@
 module StoolTestPagesHelper
+  def already_collected(employee)
+    if @employees_dup.any?{|ed| ed.id == employee.id} then
+      group_name = @employees.find_by(student_id: employee.student_id).group.name
+      "#{group_name}にて徴収済み"
+    end
+  end
+
+  def count_in_group(group_id)
+    @employees.where(group_id: group_id).size
+  end
+
+  def is_next_group(employee, index)
+    @employees = @employees.order(:group_id, :student_id)
+    if @employees[index - 1].group_id != employee.group_id then
+      true
+    else
+      false
+    end
+  end
+
+  def size_calibration(str)
+    if str.length >= 10 then
+      "10px"
+    else
+      "14px"
+    end
+  end
+
 end
